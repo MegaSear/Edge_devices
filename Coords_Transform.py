1aacf32db7bc5eae6e11b79f5d2cecd2d058be7b
Vasiliy <102398300+MegaSear@users.noreply.github.com>
2023-12-21 23:40:11 +0700

    Add files via upload

--- Coords_Transform.py
+++ Coords_Transform.py
@@ -2,14 +2,22 @@ import numpy as np
 
 def angles_solver(point_2, L1, L2):
 
-    o = np.array([0, 0, 0])
     r = 4.7
-    dz = 8 #11 8
+    dz = 10 #11 8
     n_y = np.array([0, 1, 0])
     n_z = np.array([0, 0, 1])
     n_x = np.array([1, 0, 0])
 
-    Vec = point_2 - o
+    a = 0.13 #0.139
+    b = -10 #-0.4
+    f1 = 10#8.4852813
+    radius = np.sqrt(point_2[0]**2 + point_2[1]**2)
+    print(point_2)
+    point_2 = np.array([point_2[0], point_2[1], 
+                        point_2[2] - (np.exp(a*(radius - f1)) + b)])
+    print(point_2)
+    point_2 = np.array(point_2)
+    Vec = point_2
 
     Vec_cos = np.array([Vec[0], Vec[1], 0])
     gamma = np.arccos(np.dot(Vec_cos, n_y) / (np.linalg.norm(Vec_cos) * np.linalg.norm(n_y)))  #Радианы
@@ -17,7 +25,6 @@ def angles_solver(point_2, L1, L2):
     r_vec = np.array([r* np.sin(gamma), r* np.cos(gamma), dz])
 
     point_1 = np.array(r_vec)
-    point_2 = np.array(point_2)
 
     V = point_2 - point_1
     L3 = np.linalg.norm(V)
@@ -52,7 +59,7 @@ def angles_solver(point_2, L1, L2):
     alpha = np.degrees(alpha)
     beta = np.degrees(beta)
     gamma = np.degrees(gamma)
-    fi1 = (180 - alpha)#*(2/3)
+    fi1 = (180 - alpha) + 22#*(2/3)
     fi2 = (alpha + beta - 70)#*(2/3)
     fi3 = gamma*(2/3)
     return fi1, fi2, fi3
